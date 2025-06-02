5c602c6dd6469a40e4ba41ba6940192cd1cf6408
parthkoko <parthdhaduk33@gmail.com>
2025-06-02 18:02:48 +0530

    responsive page add

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import './responsive.css';
+import './assets/styles/responsive.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
